84d393d82bb43236e032ff0cca6434ce5be2045b
Juan C Lopez <juancalpz@yahoo.com>
2024-09-05 17:44:15 -0400

    Add: 0-lockboxes.py

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    0-lockboxes.py
+
+    This module has a function canUnlockAll(boxes)
+    that determines if all lockboxes can be opened
+
+"""
+def canUnlockAll(boxes):
+    """
+        Determines if boxes can be opened
+    """
+    
+    n = len(boxes)
+    unlocked = [False] * n  # Track the unlocked boxes
+    unlocked[0] = True  # The first box is initially unlocked
+    keys = [0]  # Start with the key to the first box
+
+    while keys:
+        current_key = keys.pop(0)
+        for key in boxes[current_key]:
+            if key < n and not unlocked[key]:  # If the key unlocks a new box
+                unlocked[key] = True
+                keys.append(key)  # Add the key to explore further boxes
+
+    return all(unlocked)  # Return True if all boxes are unlocked
