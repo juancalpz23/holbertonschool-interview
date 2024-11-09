2b8856dba0c4f51bc4fa71e4d0ce2d90fc447b1d
Juan C Lopez <juancalpz@yahoo.com>
2024-11-09 01:39:42 -0400

    Update: fix 1

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,6 +1,52 @@
 #include "slide_line.h"
 
 /**
+ * shift_left - Shifts non-zero values to the left in the array.
+ * @line: Array of integers.
+ * @size: Size of the array.
+ */
+void shift_left(int *line, size_t size)
+{
+	size_t i, j = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (line[i] != 0)
+		{
+			if (j < i)
+			{
+				line[j] = line[i];
+				line[i] = 0;
+			}
+			j++;
+		}
+	}
+}
+
+/**
+ * shift_right - Shifts non-zero values to the right in the array.
+ * @line: Array of integers.
+ * @size: Size of the array.
+ */
+void shift_right(int *line, size_t size)
+{
+	int i, j = size - 1;
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		if (line[i] != 0)
+		{
+			if (j > i)
+			{
+				line[j] = line[i];
+				line[i] = 0;
+			}
+			j--;
+		}
+	}
+}
+
+/**
  * slide_line - Slides and merges an array based on direction.
  *
  * @line: Array of integers.
@@ -11,38 +57,37 @@
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	int i, j;
+	size_t i;
 
 	if (!line || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
-		return (0); /* Return failure if invalid input or direction */
+		return (0);
 
 	if (direction == SLIDE_LEFT)
 	{
-		for (i = 0; i < (int)size; i++)
-			if (line[i] == 0)
-				for (j = i + 1; j < (int)size && line[j] == 0; j++)
-					;
-		for (i = 0; i < (int)size - 1; i++)
+		shift_left(line, size);
+		for (i = 0; i < size - 1; i++)
+		{
 			if (line[i] == line[i + 1] && line[i] != 0)
 			{
 				line[i] *= 2;
 				line[i + 1] = 0;
-				i++;
 			}
+		}
+		shift_left(line, size);
 	}
 	else
 	{
-		for (i = (int)size - 1; i >= 0; i--)
-			if (line[i] == 0)
-				for (j = i - 1; j >= 0 && line[j] == 0; j--)
-					;
-		for (i = (int)size - 1; i > 0; i--)
+		shift_right(line, size);
+		for (i = size - 1; i > 0; i--)
+		{
 			if (line[i] == line[i - 1] && line[i] != 0)
 			{
 				line[i] *= 2;
 				line[i - 1] = 0;
-				i--;
 			}
+		}
+		shift_right(line, size);
 	}
-	return (1);
+
+return (1);
 }
